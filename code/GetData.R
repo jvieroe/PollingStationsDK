5fa6546db592ac2fd5463fa63c67bb4bd08515dc
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-17 12:03:03 +0100

    Update GetData.R

--- code/GetData.R
+++ code/GetData.R
@@ -1,12 +1,27 @@
 library(sf)
 library(tmap)
+library(janitor)
 
 url <- "https://api.dataforsyningen.dk/afstemningsomraader?format=geojson" 
 
 polling <- read_sf(url)
+
+polling_clnd <- polling %>% 
+  clean_names()
+
 
 
 tmap_mode("view")
 tm_shape(polling) +
   tm_polygons(col = "blue", alpha = .3)
 
+
+
+getwd()
+st_write(polling_clnd,
+         dsn = "data",
+         layer = "polling_stations",
+         factorsAsCharacter = FALSE,
+         delete_layer = TRUE,
+         layer_options = "ENCODING=UTF-8",
+         driver = "ESRI Shapefile")
