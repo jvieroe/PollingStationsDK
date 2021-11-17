00fe9f2c5e17d27107f9d95da9fd6ea29b84de4b
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-17 12:19:13 +0100

    Update GetData.R

--- code/GetData.R
+++ code/GetData.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(sf)
 library(tmap)
 library(janitor)
@@ -10,18 +11,29 @@ polling_clnd <- polling %>%
   clean_names()
 
 
-
 tmap_mode("view")
 tm_shape(polling) +
   tm_polygons(col = "blue", alpha = .3)
 
 
+# polling_clnd <- polling_clnd %>% 
+#   mutate(dagi_ID = dagi_id) %>% 
+#   select(-dagi_id)
+# 
+# polling_clnd <- polling_clnd %>% 
+#   select(c(dagi_ID,
+#            setdiff(names(polling_clnd),
+#                    "dagi_ID")
+#            )
+#          )
 
-getwd()
+
 st_write(polling_clnd,
          dsn = "data",
          layer = "polling_stations",
-         factorsAsCharacter = FALSE,
+         #factorsAsCharacter = FALSE,
          delete_layer = TRUE,
+         overwrite = TRUE,
          layer_options = "ENCODING=UTF-8",
          driver = "ESRI Shapefile")
+
